91582858769f0c1acc842f7c8d545d73132fb538
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-07-18 09:16:29 -0400

    uptade: accepted

--- python/iniciante/d1149.py
+++ python/iniciante/d1149.py
@@ -11,4 +11,16 @@ Saída
 A saída contém apenas um valor inteiro.
 """
 
+valores = list(map(int, input().split()))
 
+A = valores[0]
+N = None
+
+for valor in valores[1:]:
+    if valor > 0:
+        N = valor
+        break
+
+soma = sum(A + i for i in range(N))
+
+print(soma)
