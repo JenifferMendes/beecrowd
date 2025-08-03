74831a8feb0a952deb83274d6a8f25bb82a239ce
Jeniffer Mendes <jenih.mendes@gmail.com>
2025-08-03 14:00:47 -0400

    feat: accepted

---
+++ python/iniciante/d2850.py
@@ -0,0 +1,32 @@
+"""
+Humberto tem um papagaio muito esperto. Quando está com as duas pernas no chão,
+o papagaio fala em português. Quando levanta a perna esquerda, fala em inglês.
+Por fim, quando levanta a direita fala em francês. Nico, amigo de Humberto, 
+ficou fascinado com o animal. Em sua emoção perguntou: 
+“E quando ele levanta as duas?”. Antes que Humberto pudesse responder, 
+o papagaio gritou: “Ai eu caio, idiota!”.
+
+Entrada
+A entrada consiste de diversos casos de teste. Cada caso de teste consiste 
+uma string informando qual a situação de levantamento de pernas do papagaio.
+
+Saída
+Para cada condição de levantamento de pernas do papagaio, imprima a linguagem 
+que ele utilizará. Caso ele levante as duas pernas, imprima “caiu”. 
+Quebre uma linha a cada caso de teste.
+"""
+
+
+try:
+    while True:
+        situacao = input()
+        if situacao == 'as duas':
+            print('caiu')
+        elif situacao == 'esquerda':
+            print('ingles')
+        elif situacao == 'direita':
+            print('frances')
+        elif situacao == 'nenhuma':
+            print('portugues')
+except EOFError:
+    pass
